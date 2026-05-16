c67672398ec7085a155fed2be839f0c73548d1c8
Berthod Billy <billy.berthod@gmail.com>
2026-05-17 00:32:34 +0200

    chore(deps): add swift-snapshot-testing 1.17+ for SwiftUI snapshot tests.

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 let package = Package(
     name: "ClaudeBar",
     platforms: [.macOS(.v14)],
+    dependencies: [
+        .package(
+            url: "https://github.com/pointfreeco/swift-snapshot-testing",
+            from: "1.17.0"
+        ),
+    ],
     targets: [
         .target(
             name: "ClaudeBarLib",
@@ -16,7 +22,10 @@ let package = Package(
         ),
         .testTarget(
             name: "ClaudeBarTests",
-            dependencies: ["ClaudeBarLib"],
+            dependencies: [
+                "ClaudeBarLib",
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
+            ],
             path: "Tests/ClaudeBarTests"
         )
     ]
